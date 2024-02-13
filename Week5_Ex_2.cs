6d4956dfc106eaa83dba4886c2dfa76bcdb1ff8b
Tanishka1405 <tanishka14004@gmail.com>
2024-02-12 23:41:48 -0500

    first commit

---
+++ Week5_Ex_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write(" Please enter a whole number: ");
+        int number = int.Parse(Console.ReadLine());
+
+            // Checking if the number is positive, negative, or zero
+            if (number > 0)
+            {
+                Console.Write("The given number is positive");
+            }
+            else if (number < 0)
+            {
+                Console.Write("The given number is negative");
+            }
+            else
+            {
+                Console.Write("The given number is zero");
+            }
+
+            // Check if the number is odd or even
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(" and even.");
+            }
+            else
+            {
+                Console.WriteLine(" and odd.");
+            }
+    }
+}
